11f2e523139f74e7438d0409ed300bca39df4391
fl-tech-design <fl-tech-design@gmx.ch>
2024-10-30 23:16:00 +0100

    debug rpi code

--- ReBox_Rpi/controller_RetroBox.py
+++ ReBox_Rpi/controller_RetroBox.py
@@ -4,13 +4,6 @@ ser = serial.Serial('/dev/serial0', 9600)
 
 
 
-sticks = {
-    'xStick1': e.ABS_X,
-    'yStick1': e.ABS_Y,
-    'xStick2': e.ABS_RX,
-    'yStick2': e.ABS_RY,
-}
-
 while True:
     if ser.in_waiting > 0:
         data = ser.readline().decode().strip().split(',')
@@ -24,9 +17,4 @@ while True:
         xStick2 = int(data[2])
         yStick2 = int(data[3])
 
-        ui.write(e.EV_ABS, sticks['xStick1'], xStick1)
-        ui.write(e.EV_ABS, sticks['yStick1'], yStick1)
-        ui.write(e.EV_ABS, sticks['xStick2'], xStick2)
-        ui.write(e.EV_ABS, sticks['yStick2'], yStick2)
-
-        ui.syn()
+        print(f"{xStick1}, {yStick1}, {xStick2}, {yStick2}")
