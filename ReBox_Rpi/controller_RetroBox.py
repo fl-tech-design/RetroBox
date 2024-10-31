90ea9a6d66e9667721c79751e2f08a86bdce883e
fl-tech-design <fl-tech-design@gmx.ch>
2024-10-31 23:08:18 +0100

    upd states

--- ReBox_Rpi/controller_RetroBox.py
+++ ReBox_Rpi/controller_RetroBox.py
@@ -40,7 +40,7 @@ try:
                 
                 # Auswertung der Zustände
                 if i < len(button_pins) - 2:  # Öffner: LOW wenn gedrückt
-                    if buttons_state[i] == GPIO.HIGH:
+                    if buttons_state[i] == GPIO.LOW:
                         print(f"Button {i+1} (Öffner) gedrückt")
                 else:  # Schließer: HIGH wenn gedrückt
                     if buttons_state[i] == GPIO.HIGH:
