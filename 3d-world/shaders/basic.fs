85ece8d7d9a0968dfed14918518d309f23769627
fl-tech-design <fl-tech-design@gmx.ch>
2024-11-18 10:41:54 +0100

    change shader to 300 es

--- 3d-world/shaders/basic.fs
+++ 3d-world/shaders/basic.fs
@@ -1,8 +1,10 @@
-#version 330 core
+#version 300 es
+precision highp float;  // Setze Präzision für OpenGL ES
 
-layout(location = 0) out vec4 color;
+in vec4 fragColor;  // Eingabe aus dem Vertex-Shader (falls benötigt)
+out vec4 color;  // Ausgabe der Farbe
 
 void main()
 {
-    color = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+    color = vec4(1.0, 1.0, 0.0, 1.0);  // Gelb
 }
--- 3d-world/shaders/basic.vs
+++ 3d-world/shaders/basic.vs
@@ -1,8 +1,9 @@
-#version 330 core
+#version 300 es
+precision highp float;  // Wichtig für Präzision in OpenGL ES
 
 layout(location = 0) in vec3 position;
 
 void main()
 {
-    gl_Position = vec4(position, 1.0f);
-} 
+    gl_Position = vec4(position, 1.0);
+}
