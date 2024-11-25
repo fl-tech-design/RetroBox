a77c7e4260352bb39730ebca2a07e8a2a171a177
fl-tech-design <fl-tech-design@gmx.ch>
2024-11-25 17:29:30 +0100

    rm balck and white mod

--- 3d-world/shaders_old/postprocess.fs
+++ 3d-world/shaders_old/postprocess.fs
@@ -8,5 +8,5 @@ void main()
 {
     vec4 color = texture2D(u_texture, v_tex_coords);
     float average = (color.r + color.g + color.b) / 3.0f;
-    gl_FragColor = vec4(average, average, average, color.a);
+    gl_FragColor = color;
 }
